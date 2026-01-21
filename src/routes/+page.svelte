680c05092a42a735c8eba732553542d2c03ee2ab
Zachary Stence <zachstence@gmail.com>
2026-01-20 20:00:27 -0600

    feat: temp landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Icon } from '@steeze-ui/svelte-icon';
+	import { Tiktok } from '@steeze-ui/simple-icons';
+</script>
+
+<main class="flex h-screen w-screen flex-col items-center justify-center bg-[#d7d2cf]">
+	<h1 class="text-center text-4xl font-semibold">Lovebound Press</h1>
+	<span class="mt-2 text-lg">Coming soon!</span>
+	<div class="mt-12 flex h-12 items-center gap-4">
+		<a
+			href="https://tiktok.com/@loveboundpress"
+			target="_blank"
+			aria-label="TikTok"
+			class="block h-full rounded-md p-2 transition-all hover:bg-black/5"
+		>
+			<Icon src={Tiktok} />
+		</a>
+	</div>
+</main>
