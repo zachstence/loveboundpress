a9e465b39b4997d4423d943f1f57fda0de7bdc56
Zachary Stence <zachstence@gmail.com>
2026-01-22 17:40:27 -0600

    feat: add etsy icon link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
-	import { Icon } from '@steeze-ui/svelte-icon';
-	import { Tiktok } from '@steeze-ui/simple-icons';
+	import { Icon, type IconSource } from '@steeze-ui/svelte-icon';
+	import { Etsy, Tiktok } from '@steeze-ui/simple-icons';
 </script>
+
+{#snippet linkIcon({ href, src, label }: { href: string; src: IconSource; label: string })}
+	<a
+		{href}
+		target="_blank"
+		aria-label={label}
+		class="block h-full rounded-md p-2 transition-all hover:bg-black/5"
+	>
+		<Icon {src} />
+	</a>
+{/snippet}
 
 <main class="flex h-screen w-screen flex-col items-center justify-center bg-[#d7d2cf]">
 	<h1 class="text-center text-4xl font-semibold">Lovebound Press</h1>
 	<span class="mt-2 text-lg">Coming soon!</span>
 	<div class="mt-12 flex h-12 items-center gap-4">
-		<a
-			href="https://tiktok.com/@loveboundpress"
-			target="_blank"
-			aria-label="TikTok"
-			class="block h-full rounded-md p-2 transition-all hover:bg-black/5"
-		>
-			<Icon src={Tiktok} />
-		</a>
+		{@render linkIcon({ label: 'TikTok', src: Tiktok, href: 'https://tiktok.com/@loveboundpress' })}
+		{@render linkIcon({
+			label: 'Etsy',
+			src: Etsy,
+			href: 'https://www.etsy.com/shop/loveboundpress'
+		})}
 	</div>
 </main>
