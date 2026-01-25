4dc07c9251f1e0f9aa666eae600e75cce38fed1b
Zachary Stence <zachstence@gmail.com>
2026-01-24 21:37:47 -0600

    feat: logo and fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,60 @@
 <script lang="ts">
 	import { Icon, type IconSource } from '@steeze-ui/svelte-icon';
-	import { Etsy, Instagram, Tiktok } from '@steeze-ui/simple-icons';
+	import { Etsy, Pinterest, Tiktok } from '@steeze-ui/simple-icons';
 </script>
 
-{#snippet link({ href, iconSrc, label }: { href: string; iconSrc: IconSource; label: string })}
+{#snippet link({
+	site,
+	href,
+	iconSrc,
+	label
+}: {
+	site: string;
+	href: string;
+	iconSrc: IconSource;
+	label: string;
+})}
 	<a
 		{href}
 		target="_blank"
-		class="flex h-12 w-full items-center justify-start gap-4 rounded-md border border-[#ede7d9] bg-[#ede7d9] px-4 py-3 transition-all hover:bg-[#ede7d9]/50"
+		class="flex w-full items-center justify-start gap-3 rounded-md border border-[#ede7d9] bg-[#ede7d9] px-3 py-2 transition-all hover:bg-[#ede7d9]/50 sm:gap-4 sm:px-4 sm:py-3"
+		aria-label={site}
+		title={site}
 	>
-		<Icon src={iconSrc} class="size-6" />
+		<Icon src={iconSrc} class="size-4 sm:size-6" />
 		{label}
 	</a>
 {/snippet}
 
-<div class="flex h-screen w-screen flex-col items-center justify-center bg-[#fdf9f0]">
-	<main class="flex h-screen max-w-3xl flex-col items-center justify-center gap-12">
-		<h1 class="text-center text-4xl font-semibold">Lovebound Press</h1>
-		<div class="flex w-full max-w-52 flex-col items-center gap-4">
+<div
+	class="flex h-screen w-screen flex-col items-center justify-center bg-[#fdf9f0] p-4 text-[#4b4b4b]"
+>
+	<main class="flex h-screen w-full flex-col items-center justify-center gap-12">
+		<img src="/logo.svg" alt="Lovebound Press" class="w-full max-w-96" />
+		<div class="flex w-full max-w-64 flex-col items-center gap-4 text-sm sm:text-base">
 			{@render link({
-				label: '@loveboundpress',
-				iconSrc: Tiktok,
-				href: 'https://tiktok.com/@loveboundpress'
-			})}
-			{@render link({
-				label: 'LoveboundPress',
+				site: 'Etsy',
 				iconSrc: Etsy,
+				label: 'LoveboundPress',
 				href: 'https://www.etsy.com/shop/loveboundpress'
 			})}
 			{@render link({
+				site: 'TikTok',
+				iconSrc: Tiktok,
+				label: '@loveboundpress',
+				href: 'https://tiktok.com/@loveboundpress'
+			})}
+			{@render link({
+				site: 'Pinterest',
+				iconSrc: Pinterest,
+				label: 'LoveboundPress',
+				href: 'https://www.pinterest.com/LoveboundPress/'
+			})}
+			<!-- {@render link({
 				label: '@lovebound.press',
 				iconSrc: Instagram,
 				href: 'https://www.instagram.com/lovebound.press/'
-			})}
+			})} -->
 		</div>
 	</main>
 </div>
